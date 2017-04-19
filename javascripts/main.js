b85c6b170552f96a978c43eef24796f43d41b572
Heather Thacker <hhthacker@gmail.com>
2017-04-18 20:23:44 -0500

    write to dom

--- javascripts/main.js
+++ javascripts/main.js
@@ -4,6 +4,32 @@
 // ES6-ify it all!
 
 $(document).ready(function () {
+
+var outputContainer = $("#output");
+
+var writeToDOM = function (humanArray) {
+  var domString = "";
+  for (var i = 0; i < humanArray.length; i++) {
+    domString += `<div class="human row">`;
+    domString += `<div class="col-sm-4">`;
+    domString += `<img src="${humanArray[i].image}">`;
+    domString += `<p>${humanArray[i].name}</p>`;
+    domString += `</div>`;
+    domString += `<div class="col-sm-8 overflow-row">`;
+    for (var j = 0; j < humanArray[i].matches.length; j++){
+      domString += `<div class="animal">`;
+      domString += `<img src="${humanArray[i].matches[j].image}">`;
+      domString += `<p>${humanArray[i].matches[j].name}</p>`;
+      domString += `<p>${humanArray[i].matches[j].description}</p>`;
+      domString += `</div>`;
+    }
+    domString += `</div>`;
+    domString += `</div>`;
+  }
+  outputContainer.append(domString);
+};
+
+
 	var loadHumans = function () {
 		return new Promise(function(resolve, reject) {
 			$.ajax("./database/humans.JSON")
@@ -65,6 +91,18 @@ $(document).ready(function () {
 		}
 	};
 
+	var checkForKidFriendly =  function (human, pet) {
+		var hasKids = human["has-kids"];
+		var isKidFriendly = pet["kid-friendly"];
+		var isMatched = true;
+		if (hasKids && !isKidFriendly) {
+			isMatched = false;
+		}
+		return isMatched;
+	};
+
+
+
 	loadHumans().then(function(humans){
 		humans.forEach(function(human){
 			human.matches = [];
@@ -80,12 +118,12 @@ $(document).ready(function () {
 
 			for (var i = 0; i < myHumans.length; i++) {
 				for (var j = 0; j < myAnimals.length; j++) {
-					if (checkForTypeMatch(myHumans[i], myAnimals[j])) {
+					if (checkForTypeMatch(myHumans[i], myAnimals[j]) && checkForKidFriendly(myHumans[i], myAnimals[j])) {
 						myHumans[i].matches.push(myAnimals[j]);
 					}
 				}
 			}
-			console.log(myHumans);
+			writeToDOM(myHumans);
 		});
 	});
 

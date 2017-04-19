e397f801e9b30b941e84b31132ed3c2a2717e195
Sanyyouisf <sanymamdoh@yahoo.com>
2017-04-18 21:13:32 -0500

    done with es6

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,21 +1,26 @@
 // Use AJAX | Promises to load all 3 JSON files
 // Iterate over all JSON files and match the human with their appropriate pet(s)
-// ES6-ify it all!
+// ES6-ify it all! 
+	//no mor var (let / const)
+		//let for variable that may change 
+		// const for variable that may change
+	//PHAT  Arrow
+
 $(document).ready(function() {
 
 
-    var outputContainer = $("#output");
+    const outputContainer = $("#output");
 
-    var writeToDOM = function(humanArray) {
-        var domString = "";
-        for (var i = 0; i < humanArray.length; i++) {
+    const writeToDOM = (humanArray) => {
+        let domString = "";
+        for (let i = 0; i < humanArray.length; i++) {
             domString += `<div class="human row">`
             domString += `<div class="col-sm-4">`
             domString += `<img src="${humanArray[i].image}">`
             domString += `<p>${humanArray[i].name}</p>`
             domString += `</div>`
             domString += `<div class="col-sm-8 overflow-row">`
-            for (var j = 0; j < humanArray[i].matches.length; j++) {
+            for (let j = 0; j < humanArray[i].matches.length; j++) {
                 domString += `<div class="animal">`
                 domString += `<img src="${humanArray[i].matches[j].image}">`
                 domString += `<p>${humanArray[i].matches[j].name}</p>`
@@ -30,60 +35,57 @@ $(document).ready(function() {
 
 
 
-    var loadHumans = function() {
-        return new Promise(function(resolve, reject) {
+    // const loadHumans = function() {
+    //     return new Promise(function(resolve, reject) {
+    //         $.ajax("./database/humans.json")
+    //             .done(function(data1) {
+    //                 resolve(data1.humans);
+    //             })
+    //             .fail(function(error) {
+    //                 reject(error);
+    //             });
+    //     });
+    // };
+
+
+    const loadHumans = () => {
+        return new Promise((resolve, reject) => {
             $.ajax("./database/humans.json")
-                .done(function(data1) {
-                    resolve(data1.humans);
-                })
-                .fail(function(error) {
-                    reject(error);
-                });
+            .done((data1) => resolve(data1.humans))          
+            .fail((error) => reject(error));
         });
     };
 
-    var loadDogs = function() {
-        return new Promise(function(resolve, reject) {
-            $.ajax("./database/dogs.json")
-                .done(function(data1) {
-                    resolve(data1.dogs);
-                })
-                .fail(function(error) {
-                    reject(error);
-                });
+    const loadDogs = () => {
+        return new Promise((resolve, reject) => {
+           $.ajax("./database/dogs.json")
+            .done((data1) => resolve(data1.dogs))
+            .fail((error) => reject(error));
         });
     };
 
-    var loadCats = function() {
-        return new Promise(function(resolve, reject) {
-            $.ajax("./database/cats.json")
-                .done(function(data1) {
-                    resolve(data1.cats);
-                })
-                .fail(function(error) {
-                    reject(error);
-                });
+    const loadCats = () => {
+        return new Promise((resolve, reject) => {
+           $.ajax("./database/cats.json")
+            .done((data1) => resolve(data1.cats))
+            .fail((error) => reject(error));
         });
     };
 
-    var loadDinos = function() {
-        return new Promise(function(resolve, reject) {
-            $.ajax("./database/dinos.json")
-                .done(function(data1) {
-                    resolve(data1.dinos);
-                })
-                .fail(function(error) {
-                    reject(error);
-                });
+    const loadDinos = () => {
+        return new Promise((resolve, reject) => {
+           $.ajax("./database/dinos.json")
+            .done((data1) => resolve(data1.dinos))
+            .fail((error) => reject(error));
         });
     };
 
-    var myHumans = [];
-    var myAnimals = [];
+    let myHumans = [];
+    let myAnimals = [];
 
-    var checkForTypeMatch = function(human, pet) {
-        var interestedUnArray = human["interested-in"];
-        var isMatchNumber = interestedUnArray.indexOf(pet.type);
+    const checkForTypeMatch = (human, pet) => {
+        const interestedUnArray = human["interested-in"];
+        const isMatchNumber = interestedUnArray.indexOf(pet.type);
         if (isMatchNumber === -1) {
             return false;
         } else {
@@ -92,10 +94,10 @@ $(document).ready(function() {
     };
 
 
-    var checkForKidFriendly = function(human, pet) {
-        var hasKids = human["has-kids"];
-        var isKidFriendly = pet["kid-friendly"];
-        var isMatched = true;
+    const checkForKidFriendly = (human, pet) => {
+        const hasKids = human["has-kids"];
+        const isKidFriendly = pet["kid-friendly"];
+        let isMatched = true;
         if (hasKids && !isKidFriendly) {
             isMatched = false;
         }
@@ -105,27 +107,21 @@ $(document).ready(function() {
 
     loadHumans()
         .then(function(humans) {
-            humans.forEach(function(human) {
+            humans.forEach((human) => {
                 human.matches = [];
                 myHumans.push(human);
             });
             Promise.all([loadDogs(), loadCats(), loadDinos()])
-                .then(function(result) {
+                .then((result) => {
                     // console.log(result);
-                    result.forEach(function(xhresult) {
-                        xhresult.forEach(function(animal) {
-                            // console.log("xhresult",xhresult);
-                            myAnimals.push(animal);
-                        });
+                    result.forEach((xhresult) => {
+                        xhresult.forEach( (animal) => myAnimals.push(animal));
                     });
 
-                    for (var i = 0; i < myHumans.length; i++) {
-                        for (var j = 0; j < myAnimals.length; j++) {
+                    for (let i = 0; i < myHumans.length; i++) {
+                        for (let j = 0; j < myAnimals.length; j++) {
                             if (checkForTypeMatch(myHumans[i], myAnimals[j]) && checkForKidFriendly(myHumans[i], myAnimals[j])) {
-                                // console.log(myHumans[i]);
-                                // myHumans[i].matches(myAnimals[j]);
                                 myHumans[i].matches.push(myAnimals[j]);
-                                // console.log(checkForTypeMatch((myHumans[i],myAnimals[j]);
                             }
                         }
                     }
@@ -139,9 +135,6 @@ $(document).ready(function() {
         .catch(function(humanError) {
             console.log(humanError);
         });
-
-
-
 
 
 });
